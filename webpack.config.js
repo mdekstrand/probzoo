5f92a558df90f88267c6a62b5a98f43eecf333ac
Michael Ekstrand <md@ekstrandom.net>
2020-07-09 13:19:08 -0600

    a little webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "build/site");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: dist,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};
